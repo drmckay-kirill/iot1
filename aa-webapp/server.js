62c90c2108a78772c3b41d5dd0077c9169539017
Kirill Yurevich Ponomarev <drmckay-kirill@yandex.ru>
2018-05-10 20:57:22 +0500

    use ejs for front webapp

--- aa-webapp/server.js
+++ aa-webapp/server.js
@@ -3,6 +3,7 @@ var OAuth2 = require('./oauth2').OAuth2;
 var config = require('./config');
 var session = require('express-session');
 var http = require("http");
+const title = "TEST";
 
 // Express configuration
 var app = express();
@@ -14,12 +15,10 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-
-app.configure(function () {
+app.set("view engine", "ejs");
+app.configure( () => {
     "use strict";
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    //app.use(express.logger());
-    app.use(express.static(__dirname + '/public'));
 });
 
 // Config data from config.js file
@@ -41,12 +40,11 @@ function getJSON (rest, method, access_token, onResult) {
         }
     };
     
-    var req = http.request(options, function(res)
-    {
+    var req = http.request(options, (res) => {
         var output = '';
         res.setEncoding('utf8');
 
-        res.on('data', function (chunk) {
+        res.on('data', (chunk) => {
             output += chunk;
         });
 
@@ -56,11 +54,21 @@ function getJSON (rest, method, access_token, onResult) {
         });
     });
 
-    req.on('error', function(err) {
+    req.on('error', (err) => {
         console.log('error: ' + err.message);
     });
 
     req.end();
+};
+
+function renderVariables() {
+    return {
+        "title": title,
+        "access_token": "",
+        "displayName": undefined,
+        "email": undefined,
+        "attributes": []
+    }  
 };
 
 // Creates oauth library object with the config data
@@ -72,30 +80,27 @@ var oa = new OAuth2(client_id,
                     callbackURL);
 
 // Handles requests to the main page
-app.get('/', function(req, res) {
-    console.log(req.session);
-
+app.get('/', (req, res) => {
     // If auth_token is not stored in a session cookie it sends a button to redirect to IDM authentication portal 
     if(!req.session.access_token) {
-        res.send("Oauth2 IDM Demo.<br><br><button onclick='window.location.href=\"/auth\"'>Log in with FI-WARE Account</button>");
-
-    // If auth_token is stored in a session cookie it sends a button to get user info
+        res.render("login", {
+            "title":  title
+        });
     } else {
-        res.send("Successfully authenticated. <br><br> Your oauth access_token: " + req.session.access_token + "<br><br><button onclick='window.location.href=\"/user_info\"'>Get my user info</button><br><br><button onclick='window.location.href=\"/logout\"'>Logout</button>");
+        renderStructure = renderVariables();
+        renderStructure.access_token = req.session.access_token;
+        res.render("index", renderStructure);
     }
 });
 
 // Handles requests from IDM with the access code
-app.get('/login', function(req, res) {
-    console.log(req.session);
+app.get('/login', (req, res) => {
     if (req.query.code) {
         // Using the access code goes again to the IDM to obtain the access_token
         oa.getOAuthAccessToken(req.query.code, function (e, results) {
             // Stores the access_token in a session cookie
             req.session.access_token = results.access_token;
-
             res.redirect('/');
-
         });
     } else {
         res.redirect('/');
@@ -103,32 +108,32 @@ app.get('/login', function(req, res) {
 });
 
 // Redirection to IDM authentication portal
-app.get('/auth', function(req, res) {
-    console.log(req.session);
+app.get('/auth', (req, res) => {
     var path = oa.getAuthorizeUrl(response_type);
     res.redirect(path);
 });
 
 // Ask IDM for user info
 app.get('/user_info', function(req, res){
-    console.log(req.session);
     if(req.session.access_token) {
         var url = config.idmURL + '/user';
 
         // Using the access token asks the IDM for the user info
         oa.get(url, req.session.access_token, function (response) {
-            //console.log(response);
             var user = JSON.parse(response.data);
-            res.send("Welcome " + user.displayName + "<br> Your email address is " + user.email + "<br><br><button onclick='window.location.href=\"/\"'>Main page</button>");
+            renderStructure = renderVariables();
+            renderStructure.access_token = req.session.access_token;
+            renderStructure.displayName = user.displayName;
+            renderStructure.email = user.email;
+            res.render("index", renderStructure);
         });
     } else {
-        res.send("Access token does not exists!<br><br><button onclick='window.location.href=\"/\"'>Main page</button>");
+        res.send("Not authenticated", 401);
     }
 });
 
 // Handles logout requests to remove access_token from the session cookie
 app.get('/logout', function(req, res) {
-    console.log(req.session);
     req.session.access_token = undefined;
     res.redirect('/');
 });
@@ -137,7 +142,10 @@ app.get('/attributes', function (req, res) {
     if (req.session.access_token) {
         
         getJSON('/attributes', 'GET', req.session.access_token, function (status, response) {
-            res.send(response);
+            renderStructure = renderVariables();
+            renderStructure.access_token = req.session.access_token;
+            renderStructure.attributes = response;
+            res.render("index", renderStructure);          
         });
         
     } else {
@@ -145,5 +153,6 @@ app.get('/attributes', function (req, res) {
     }
 });
 
-console.log('Server listen in port 80. Connect to localhost');
-app.listen(80);
+app.listen(80, () => {
+    console.log('Server listen in port 80. Connect to localhost...');
+});
